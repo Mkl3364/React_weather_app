8dcbabb08306ee43a36f2e56cdec5edc006ec57c
Mickael Brunet <mickael.brunet64@gmail.com>
2022-06-18 23:52:14 +0200

    Put token in .env

--- src/logic/api/getWeather.ts
+++ src/logic/api/getWeather.ts
@@ -6,7 +6,7 @@ export default class WeatherAPI {
     public readonly location: string
     public readonly lang: string
     private baseUrl: string = 'https://api.openweathermap.org/data/2.5/weather'
-    private appId: string = '83b97d9eb53f111b47350729a6a40d31'
+    private appId: string = process.env.REACT_APP_OPEN_WEATHER_APPID!
     private units?: string = "metric"
 
     constructor(location: string, lang: string, units?: string) {
